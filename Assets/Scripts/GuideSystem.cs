437168356eaa8a4fea6959d913b2873ae773fdaa
Ordyns <88380021+Ordyns@users.noreply.github.com>
2022-08-23 23:50:07 +0300

    GuideSystem rework
    GuideSystem is split into GuideSystem and GuideView
    GuideView contains guide steps and shows them

--- Assets/Scripts/GuideSystem.cs
+++ Assets/Scripts/GuideSystem.cs
@@ -2,128 +2,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.Video;
+using System;
 
 [RequireComponent(typeof(VideoPlayer))]
 public class GuideSystem : MonoBehaviour
 {
     public bool IsGuideShowing { get; private set; }
     public event System.Action GuideFinished;
-
-    [SerializeField] private List<Guide> guides;
-    [Space]
-    [SerializeField] private GameObject guidesParent;
+    
     [SerializeField] private CanvasGroup background;
     [SerializeField] private AnimatedPanel continueButton;
+    [field:Space]
+    [field:SerializeField] public GuideView[] GuideViews { get; private set; }
 
     private VideoPlayer _videoPlayer;
 
-    private Guide _currentGuide;
-    private GuideStep _currentGuideStep => _currentGuide.Steps[_currentGuideStepIndex];
-    private int _currentGuideStepIndex;
+    private GuideView _currentGuideView;
+    private SaveSystem _saveSystem;
 
     private void Start() {
         _videoPlayer = GetComponent<VideoPlayer>();
+        _saveSystem = SaveSystem.Instance;
+
         IsGuideShowing = false;
 
-        foreach(Guide guide in guides){
-            guide.Parent.transform.gameObject.SetActive(false);
+        foreach(GuideView guideView in GuideViews){
+            if(guideView.TargetLevelNumber == ScenesLoader.Instance.LastLoadedLevelNumber)
+                _currentGuideView = guideView;
 
-            for(int j = 0; j < guide.Steps.Length; j++){
-                foreach(AnimatedPanel animatedPanel in guide.Steps[j].AnimatedPanels){
-                    animatedPanel.gameObject.SetActive(true);
-                }
-            }
+            guideView.gameObject.SetActive(false);
         }
 
         background.alpha = 0;
-        guidesParent.SetActive(false);
-
-        Guide currentLevelGuide = guides.Find(guide => guide.TargetLevelNumber == ScenesLoader.Instance.LastLoadedLevelNumber);
-        if(currentLevelGuide != null){
-            bool guideCompleted = SaveSystem.Instance.Data.CompletedLevelsWithGuides.Contains(currentLevelGuide.TargetLevelNumber);
-            if(guideCompleted == false){
-                StartGuide(currentLevelGuide);
-                return;
-            }
-        }
+        background.gameObject.SetActive(false);
         
+        if(_currentGuideView && _saveSystem.Data.CompletedLevelsWithGuides.Contains(_currentGuideView.TargetLevelNumber) == false)
+            StartGuide(_currentGuideView);
+        else
+            GuideFinished?.Invoke();
+    }
+
+    private void StartGuide(GuideView guideView){
+        _currentGuideView.gameObject.SetActive(true);
+        _currentGuideView.StartGuide(continueButton, PlayGuideVideo);
+        
+        GuideFinished += () => _saveSystem.Data.CompletedLevelsWithGuides.Add(_currentGuideView.TargetLevelNumber);
+
+        background.gameObject.SetActive(true);
+        background.DOFade(1, 0.25f).SetEase(Ease.InOutSine);
+    }
+
+    public void MoveToNextGuideStep(){
+        _videoPlayer.Stop();
+        IsGuideShowing = true;
+
+        if(_currentGuideView.MoveToNextStep() == false)
+            FinishGuide();
+    }
+
+    private void FinishGuide(){
+        _currentGuideView.CloseGuide();
+        continueButton.Close();
+        background.DOFade(0, 0.2f).SetEase(Ease.InOutSine);
+
+        IsGuideShowing = false;
         GuideFinished?.Invoke();
     }
 
-    private void StartGuide(Guide guide){
-        IsGuideShowing = true;
-        _currentGuide = guide;
-        _currentGuideStepIndex = -1;
-
-        guidesParent.SetActive(true);
-        guide.Parent.transform.gameObject.SetActive(true);
-
-        background.DOFade(1, 0.25f).SetEase(Ease.InOutSine);
-
-        GuideFinished += () => SaveSystem.Instance.Data.CompletedLevelsWithGuides.Add(_currentGuide.TargetLevelNumber);
-
-        NextGuideStep();
-    }
-
-    public void NextGuideStep(){
-        _currentGuideStepIndex++;
-        
-        continueButton.Close();
-        _videoPlayer.Stop();
-
-        if(_currentGuideStepIndex >= _currentGuide.Steps.Length){
-            FinishGuide();
-            return;
-        }
-
-        if(_currentGuideStepIndex >= 1 && _currentGuideStep.ClosePreviousSteps){
-            foreach(AnimatedPanel animatedPanel in _currentGuide.Steps[_currentGuideStepIndex - 1].AnimatedPanels){
-                animatedPanel.Close();
-            }
-        }
-
-        foreach(AnimatedPanel animatedPanel in _currentGuideStep.AnimatedPanels)
-            animatedPanel.Open();
-
-        PlayVideo(_currentGuideStep.VideoClip);
-
-        TimeOperations.CreateTimer(_currentGuideStep.StepDuration, null, () => continueButton.Open());
-    }
-
-    private void PlayVideo(VideoClip videoClip){
+    public void PlayGuideVideo(VideoClip videoClip){
         if(videoClip == null)
             return;
 
         _videoPlayer.targetTexture.Release();
-        _videoPlayer.clip = _currentGuideStep.VideoClip;
+        _videoPlayer.clip = videoClip;
         _videoPlayer.Play();
-    }
-
-    private void FinishGuide(){
-        IsGuideShowing = false;
-        GuideFinished?.Invoke();
-
-        _currentGuide.Parent.DOFade(0, 0.2f).SetEase(Ease.InOutSine);
-        background.DOFade(0, 0.2f).SetEase(Ease.InOutSine);
-        TimeOperations.CreateTimer(0.2f, null, () => guidesParent.SetActive(false));
-    }
-
-    [System.Serializable]
-    public class Guide{
-        public int TargetLevelNumber;
-        [Space]
-        public CanvasGroup Parent;
-        public GuideStep[] Steps;
-    }
-
-    [System.Serializable]
-    public struct GuideStep{
-        public float StepDuration;
-        public bool ClosePreviousSteps;
-        [Space]
-        public AnimatedPanel[] AnimatedPanels;
-
-        [Header("Optional")]
-        public VideoClip VideoClip;
     }
 }
---
+++ Assets/Scripts/GuideView.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class GuideView : MonoBehaviour
+{
+    [field:SerializeField] public int TargetLevelNumber { get; private set; }
+    [Space]
+    [SerializeField] private GuideStep[] steps;
+
+    private AnimatedPanel _continueButton;
+    private CanvasGroup _canvasGroup;
+
+    private int _currentGuideStepIndex;
+    private GuideStep _currentGuideStep => steps[_currentGuideStepIndex];
+
+    private System.Action<VideoClip> PlayVideoClip;
+
+    public void StartGuide(AnimatedPanel continueButton, System.Action<VideoClip> playVideoClipAction){
+        _continueButton = continueButton;
+        PlayVideoClip = playVideoClipAction;
+
+        _canvasGroup = GetComponent<CanvasGroup>();
+
+        foreach(GuideStep step in steps){
+            foreach(AnimatedPanel animatedPanel in step.AnimatedPanels){
+                animatedPanel.gameObject.SetActive(true);
+                animatedPanel.CloseInstantly();
+            }
+        }
+
+        MoveToStep(0);
+    }
+
+    public bool MoveToNextStep(){
+        _currentGuideStepIndex++;
+
+        if(_currentGuideStepIndex >= steps.Length)
+            return false;
+        
+        MoveToStep(_currentGuideStepIndex);
+
+        return true;
+    }
+
+    private void MoveToStep(int stepIndex){
+        _continueButton.Close();
+
+        GuideStep step = steps[stepIndex];
+
+        if(stepIndex >= 1 && step.ClosePreviousSteps){
+            foreach(AnimatedPanel animatedPanel in steps[stepIndex - 1].AnimatedPanels){
+                animatedPanel.Close();
+            }
+        }
+
+        foreach(AnimatedPanel animatedPanel in step.AnimatedPanels)
+            animatedPanel.Open();
+
+        if(step.VideoClip)
+            PlayVideoClip(step.VideoClip);
+
+        TimeOperations.CreateTimer(_currentGuideStep.StepDuration, null, _continueButton.Open);
+    }
+        
+    public void CloseGuide(){
+        foreach(AnimatedPanel animatedPanel in steps[steps.Length - 1].AnimatedPanels)
+            animatedPanel.Close();
+
+        _canvasGroup.DOFade(0, 0.2f).SetEase(Ease.InOutSine).OnComplete(() => gameObject.SetActive(false));
+    }
+
+    [System.Serializable]
+    public struct GuideStep {
+        public float StepDuration;
+        public bool ClosePreviousSteps;
+        [Space]
+        public AnimatedPanel[] AnimatedPanels;
+
+        [Header("Optional")]
+        public VideoClip VideoClip;
+    }
+}
